44f0b6b1c5f5ee83ff807f189dc92c658ce14a43
Eldar <tebres.eki76@gmail.com>
2025-03-30 20:55:09 +0200

    Added core URL to include yousend_core routes

--- yousend/yousend/urls.py
+++ yousend/yousend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("auth/", include("yousend_auth.urls")),
+    path("", include("yousend_core.urls")),
 ]
